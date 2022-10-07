a4a3d68b9f1c2b750280e8dfda5042601d353ce3
Abraham <abraham.lara98@gmail.com>
2022-10-07 15:30:50 -0500

    crear posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -17,8 +17,32 @@ class PostController extends Controller
         );
     }
 
+    public function create(Post $post){
+       return view('posts.create',['post' => $post]);
+    }
+
+    public function store(Request $request){
+
+        $post = $request->user()->posts()->create
+        ([
+            'tile'=> $title = $request->title,
+            'slug'=> Str::slug($title),
+            'body'=>  $request->body,
+        ]);
+
+        return redirect()->route('posts.edit', $post);
+    }
+
+    public function edit(Post $post){
+
+        return view('posts.edit', ['post' => $post]);
+       
+    }
+
     public function destroy(Post $post){
         $post->delete();
         return back();
     }
+
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -35,9 +35,9 @@ Route::get('blog/{slug}', [PageController::class,'post'])->name('post');
 
 Route::controller(PageController::class)->group(function () {
 
-  Route::get('/',           'home')->name('home');
-  Route::get('blog',        'blog')->name('blog');
-  Route::get('blog/{Post slug}',   'post')->name('post');
+  Route::get('/',                   'home')->name('home');
+  Route::get('blog',                'blog')->name('blog');
+  Route::get('blog/{post:slug}',    'post')->name('post');
 });
 
 Route::get('/dashboard', function () {
